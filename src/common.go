14a90fd9d6a43f24e421e2098cae9f6491cae8af
haojie <haojie@build22.kill.corp.qihoo.net>
2020-08-14 09:35:03 +0800

    test change

--- src/common.go
+++ src/common.go
@@ -1,10 +1,10 @@
-package main
+package common
 
 import "fmt"
 import "gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 
 
-func main(){
+func fun(){
 	fmt.Println("hello")
 	cm := kafka.ConfigMap{}
 	cm.SetKey("bootstrap.servers", "localhost:9092")
